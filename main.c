766b1a60a19c46af1fa1a08330a1f88f70e2e279
Simon TURC <simonturc@pop-os.localdomain>
2021-03-23 20:10:07 +0100

    Button with function

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include <gtk/gtk.h>
 
+static void function_test(GtkWidget *widget,gpointer data)
+{
+    g_print("Pass\n");
+}
+
 int main(int arc , char *argv[])
 {
-    GtkWidget *main_window = NULL;
-    GtkBuilder *builder = NULL;
-    GError *error = NULL;
-    gchar *filename = NULL;
+    GtkWidget *main_window ;
+    GtkButton *button_filter1 ;
+    GtkButton *button_filter2 ;
+    GtkButton *button_filter3 ;
+    GtkBuilder *builder ;
+    GError *error ;
+    gchar *filename ;
     gtk_init(NULL,NULL);
     builder = gtk_builder_new();
     filename = g_build_filename("window.glade", NULL);
@@ -17,9 +25,19 @@ int main(int arc , char *argv[])
         g_error_free(error);
         return error->code;
     }
+
     main_window = GTK_WIDGET(gtk_builder_get_object (builder, "MainWindow"));
+    button_filter1 = GTK_BUTTON(gtk_builder_get_object (builder, "filter_1"));
+    button_filter2 = GTK_BUTTON(gtk_builder_get_object (builder, "filter_2"));
+    button_filter3 = GTK_BUTTON(gtk_builder_get_object (builder, "filter_3"));
+
     g_signal_connect (G_OBJECT (main_window), "destroy", (GCallback)gtk_main_quit, NULL);
+    g_signal_connect(G_OBJECT (button_filter1), "clicked", G_CALLBACK(function_test), NULL);
+    g_signal_connect(G_OBJECT (button_filter2), "clicked", G_CALLBACK(function_test), NULL);
+    g_signal_connect(G_OBJECT (button_filter3), "clicked", G_CALLBACK(function_test), NULL);
+
     gtk_widget_show_all (main_window);
     gtk_main();
+
     return 0;
 }
