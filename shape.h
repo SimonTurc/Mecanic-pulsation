c49c968214e57243fdab8ac20b7a3f5d436f7fe2
bastien.pouessel <bastien.pouessel@epita.fr>
2021-06-12 15:24:01 +0200

    delete files

--- shape.h
+++
@@ -1,6 +0,0 @@
-#ifndef MECHANIC_PULSATION_SHAPE_H
-#define MECHANIC_PULSATION_SHAPE_H
-
-void draw_triangle(float points[], unsigned int indexes[], unsigned int buffer_size, unsigned int index_buffer_size);
-
-#endif // MECHANIC_PULSATION_SHAPE_H
